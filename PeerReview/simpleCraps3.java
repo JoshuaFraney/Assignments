40f1896492633e539375226b670e41235e1a7a75
Joshua Franey <joshua.franey@gmail.com>
2016-08-18 09:03:51 -0400

    Craps Game Assignment

--- PeerReview/simpleCraps3.java
+++ PeerReview/simpleCraps3.java
@@ -13,7 +13,7 @@ public class simpleCraps3 {
 		      int point = 0;
 		         
 		         
-		      for (int i = 0;  i<135; i = win + loss) {
+		     for (int i = 0;  i<135; i = win + loss) {
 		    	  
 		    	  dice = (int)(6.0*Math.random() + 1.0) +
 				             (int)(6.0*Math.random() + 1.0);
@@ -34,30 +34,36 @@ public class simpleCraps3 {
 		    	  
 		         int pointNumber = dice; // Rolled a number other than 2, 3, 7, or 12 so you have a Point Number
 		         System.out.println("Your Point Number is: " + pointNumber);
+		         gameover = false;
 		         
 		         while (gameover == false) {  // Roll additional times until you roll a 7 and lose or roll the Point Number and win the game
 		            dice = (int)(6.0*Math.random() + 1.0) +
 		                   (int)(6.0*Math.random() + 1.0);
 		            System.out.println("\nYour next roll is a " + dice);
-		            
-		            if (dice == pointNumber) {
+		         
+		            if (dice == pointNumber || dice != 7) {
 		               win++;
 		               gameover = true;
 		               System.out.println("\nYou Win!! Congratulations!!");
-		               break; // Win the game, Game Over
+		                // Win the game, Game Over
 		            }
 		            
 		            if (dice == 7) {
 		               loss++;
 		               gameover = true;
 		               System.out.println("\nYou rolled a 7 and lost :( Better luck next time..");
-		               break; // Lose the game, Game Over
+		                // Lose the game, Game Over
 		             
 		            }
 		           
-		            else System.out.println("You rolled a number other than the Point Number or a 7, continue rolling.."); // Statement for continuing the game
+		            if (dice != pointNumber || dice != 7) {
+		            	System.out.println("You rolled a number other than the Point Number or a 7, continue rolling.."); // Statement for continuing the game
+		            	gameover = false;
+		            	break;
+		            }
 		            
 		         }
+		         
 		         
 		         
 		      }
