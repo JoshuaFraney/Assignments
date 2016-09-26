85dab7954821d27d06cceeca92a36dbeaad7ec59
Joshua Franey <joshua.franey@gmail.com>
2016-09-26 16:38:14 -0400

    Day 31 Assignment - Major Class Hibernate

--- Day31/src/ssa/Mainline.java
+++ Day31/src/ssa/Mainline.java
@@ -10,12 +10,12 @@ public class Mainline {
 //		Major = "Medicine", required sat = 1400);
 //		Major = "PingPong", required sat = 200);
 //		Major = "Business Ethics", required sat = 850);
-		//MajorMethods.createMajor();
+		MajorMethods.createMajor();
 		//Updates 'Math' Major (id 4) to 'Mathematics' and 
 		//Updates the required sat score for 'General Studies' (major id 7) to 600 instead of 500
-	    //MajorMethods.updateMajor();
+	    MajorMethods.updateMajor();
 		//Deletes majors 11 and 12 (Ping Pong and Business Ethics consecutively)
-		//MajorMethods.deleteMajor();
+		MajorMethods.deleteMajor();
 		//Displays a list of all the majors remaining on the major table
 		MajorMethods.displayMajor();
 		
